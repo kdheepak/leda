77519fcc28c653c389772080e3c36c9d18818f4b
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-11-10 22:56:20 -0700

    Add example

--- leda/__init__.py
+++ leda/__init__.py
@@ -3,6 +3,15 @@ from jinja2 import Template
 
 _loaded = {'d3': False, 'gmaps': False}
 
+def check_loaded(script):
+    def wrapper(func):
+        def f(*args, **kwargs):
+            if _loaded[script] is not True:
+                func(*args, **kwargs)
+                _loaded[script] = True
+        return f
+    return wrapper
+
 def enable(d3=True, gmaps=False):
 
     if d3 and not _loaded['d3']:
@@ -12,7 +21,7 @@ def enable(d3=True, gmaps=False):
         enable_gmaps()
         _loaded['gmaps'] = True
 
-
+@check_loaded('d3')
 def enable_d3(version=3):
 
     string= Template("""
@@ -33,11 +42,12 @@ def enable_d3(version=3):
     display(Javascript(string))
 
 
-def enable_gmaps():
-
+@check_loaded('gmaps')
+def enable_gmaps(**kwargs):
     string = Template("""
     <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
-    """).render()
+    <script src="https://maps.googleapis.com/maps/api/js?key={{key}}&sensor=true"></script>
+    """).render(**kwargs)
     display(HTML(string))
 
 
@@ -55,3 +65,18 @@ def render_js(string, **kwargs):
     string = Template(string).render(**kwargs)
 
     display(Javascript(string))
+
+
+def render_css(string, **kwargs):
+
+    string = Template('''
+<style>
+
+{{string}}
+
+</style>
+    ''').render(string=string)
+
+    string = Template(string).render(**kwargs)
+    display(HTML(string))
+
